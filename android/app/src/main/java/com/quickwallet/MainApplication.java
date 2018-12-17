b393e00a4ac98d94894448696720e5820ff2caa4
zhoujian <jian.zhou@litex.io>
2018-12-17 19:29:58 +0800

    使用 Ignite CLI 初始化项目

--- android/app/src/main/java/com/quickwallet/MainApplication.java
+++ android/app/src/main/java/com/quickwallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.quickwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new RNI18nPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
