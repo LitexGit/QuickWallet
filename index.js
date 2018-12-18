4b289f00b1fc1df8565a54fe89a89c7f7f9d60e1
zhoujian <jian.zhou@litex.io>
2018-12-18 10:20:34 +0800

    fix(index.js): disableYellowBox
    disableYellowBox

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import './App/Config/ReactotronConfig';
-import { AppRegistry } from 'react-native';
+import { AppRegistry} from 'react-native';
 import App from './App/Containers/App';
+
+console.disableYellowBox = true;
 
 AppRegistry.registerComponent('QuickWallet', () => App);
