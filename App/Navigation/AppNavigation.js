cb680fb81358e0d5c19acf255fe51ab20d82f5ec
zhoujian <jian.zhou@litex.io>
2018-12-18 09:44:50 +0800

    ci(.eslintrc.js): eslint --init
    使用ESLint

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,18 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
-import LaunchScreen from '../Containers/LaunchScreen'
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import LaunchScreen from '../Containers/LaunchScreen';
 
-import styles from './Styles/NavigationStyles'
+import styles from './Styles/NavigationStyles';
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+	LaunchScreen: { screen: LaunchScreen }
 }, {
-  // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
-  navigationOptions: {
-    headerStyle: styles.header
-  }
-})
+	// Default config for all screens
+	headerMode: 'none',
+	initialRouteName: 'LaunchScreen',
+	navigationOptions: {
+		headerStyle: styles.header
+	}
+});
 
-export default createAppContainer(PrimaryNav)
+export default createAppContainer(PrimaryNav);
