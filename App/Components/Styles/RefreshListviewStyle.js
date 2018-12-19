39dcf6ea6c63704a8871f3b8d6222a2780eb951f
zhoujian <jian.zhou@litex.io>
2018-12-19 15:02:10 +0800

    feat(Components): 添加自定义组件 RefreshListview RefreshSectionListview
    添加自定义组件 RefreshListview RefreshSectionListview

---
+++ App/Components/Styles/RefreshListviewStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    footerContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        height: 44,
+        // backgroundColor: Colors.theme_white_color,
+        backgroundColor:'cyan'
+    },
+    footerText: {
+        // fontSize: FontSize.fontSize_14,
+        // color: Colors.title_color_002,
+        // backgroundColor: Colors.theme_white_color,
+        backgroundColor:'yellow'
+    }
+});
