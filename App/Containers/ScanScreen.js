722b7663c47f57116ca55080cacfd3bcf99cfb04
zhoujian <jian.zhou@litex.io>
2018-12-20 22:20:41 +0800

    feat(ScanScreen.js): 添加 扫描二维码 UI
    添加 扫描二维码 UI

--- App/Containers/ScanScreen.js
+++ App/Containers/ScanScreen.js
@@ -1,26 +1,73 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native';
+import { View, Text, InteractionManager, Animated, Easing, Image, Dimensions, Platform,
+    Alert,
+    Vibration } from 'react-native';
 import { connect } from 'react-redux';
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
-
-// Styles
 import styles from './Styles/ScanScreenStyle';
+import { RNCamera } from 'react-native-camera';
 
 class ScanScreen extends Component {
-  componentDidMount=()=>{
 
+  static navigationOptions = {
+      title: '扫码',
+  };
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          animation: new Animated.Value(0),
+      };
   }
-  render () {
+
+  _onBarCodeRead(e) {
+      console.log('============e========================');
+      console.log(e.data);
+  }
+
+  _startAnimation(){
+      this.state.animation.setValue(0);
+      Animated.timing(this.state.animation,{
+          toValue:1,
+          duration:1500,
+          easing:Easing.linear,
+      }).start(()=>this._startAnimation());
+  }
+
+  componentDidMount(){
+      InteractionManager.runAfterInteractions(()=>this._startAnimation());
+  }
+
+
+  render() {
+      const barcodeType = Platform.OS === 'ios' ? [RNCamera.Constants.BarCodeType.qr] : RNCamera.Constants.GoogleVisionBarcodeDetection.BarcodeType.QR_CODE;
       return (
-          <ScrollView style={styles.container}>
-              <KeyboardAvoidingView behavior='position'>
-                  <Text>ScanScreen</Text>
-              </KeyboardAvoidingView>
-          </ScrollView>
-      );
+          <RNCamera style={styles.container}
+              type={RNCamera.Constants.Type.back}
+              barCodeTypes={barcodeType}
+              flashMode={RNCamera.Constants.FlashMode.auto}
+              onBarCodeRead={(e) => this._onBarCodeRead(e)}>
+              <View style={styles.scanBeside}></View>
+              <View style={styles.scanSection}>
+                  <View style={styles.scanBeside}></View>
+                  <View style={styles.scanerView}>
+                      <Image style={styles.scaner} source={require('../Images/icon_scan_rect.png')}/>
+                      <Animated.View style={[styles.animatedLine, {
+                          transform: [{
+                              translateY: this.state.animation.interpolate({
+                                  inputRange: [0,1],
+                                  outputRange: [0,250]
+                              })
+                          }]
+                      }]}/>
+                  </View>
+                  <View style={styles.scanBeside}></View>
+              </View>
+              <View style={styles.bottomSection}></View>
+          </RNCamera>);
   }
 }
+
+
 
 const mapStateToProps = (state) => ({
 });
@@ -29,3 +76,9 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ScanScreen);
+
+
+
+
+
+
--- App/Containers/Styles/ScanScreenStyle.js
+++ App/Containers/Styles/ScanScreenStyle.js
@@ -2,5 +2,31 @@ import { StyleSheet } from 'react-native';
 import { ApplicationStyles } from '../../Themes/';
 
 export default StyleSheet.create({
-    ...ApplicationStyles.screen
+    ...ApplicationStyles.screen,
+    scanBeside:{
+        flex:1,
+        backgroundColor:'rgba(0,0,0,0.5)'
+    },
+    scanSection:{
+        height:250,
+        flexDirection:'row',
+    },
+    bottomSection:{
+        flex:1.5,
+        backgroundColor:'rgba(0,0,0,0.5)'
+    },
+    scanerView:{
+        width:250,
+    },
+    animatedLine:{
+        height:2,
+        backgroundColor:'#00c050'
+    },
+    scaner:{
+        width:250,
+        height:250,
+        position:'absolute',
+        left: 0,
+        top: 0,
+    }
 });
