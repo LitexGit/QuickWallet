662af12868c56bb8c2e9e103b51db5110b191833
zhoujian <jian.zhou@litex.io>
2018-12-21 15:56:48 +0800

    feat(.env Api.js): 添加 必要的Api 接口
    添加 必要的Api 接口

--- App/Services/Api.js
+++ App/Services/Api.js
@@ -1,64 +1,60 @@
-// a library to wrap and simplify api calls
-import apisauce from 'apisauce';
 
-// our "constructor"
-const create = (baseURL = 'https://api.github.com/') => {
-    // ------
-    // STEP 1
-    // ------
-    //
-    // Create and configure an apisauce-based api object.
-    //
+import apisauce from 'apisauce';
+import Config from 'react-native-config';
+
+const create = (baseURL = Config.API_URL) => {
     const api = apisauce.create({
-    // base URL is read from the "constructor"
         baseURL,
-        // here are some default headers
-        headers: {
-            'Cache-Control': 'no-cache'
-        },
-        // 10 second timeout...
+        headers: {'Cache-Control': 'no-cache'},
         timeout: 10000
     });
 
-    // ------
-    // STEP 2
-    // ------
-    //
-    // Define some functions that call the api.  The goal is to provide
-    // a thin wrapper of the api layer providing nicer feeling functions
-    // rather than "get", "post" and friends.
-    //
-    // I generally don't like wrapping the output at this level because
-    // sometimes specific actions need to be take on `403` or `401`, etc.
-    //
-    // Since we can't hide from that, we embrace it by getting out of the
-    // way at this level.
-    //
     const getRoot = () => api.get('');
     const getRate = () => api.get('rate_limit');
     const getUser = (username) => api.get('search/users', {q: username});
 
-    // ------
-    // STEP 3
-    // ------
-    //
-    // Return back a collection of functions that we would consider our
-    // interface.  Most of the time it'll be just the list of all the
-    // methods in step 2.
-    //
-    // Notice we're not returning back the `api` created in step 1?  That's
-    // because it is scoped privately.  This is one way to create truly
-    // private scoped goodies in JavaScript.
-    //
+    /**
+     * 用户信息注册接口
+     *
+     * address   用户地址
+     * type      1新建 2助记词导入 3私钥导入
+     * os        用户系统平台 ios或android
+     * phoneinfo 用户手机详细信息
+     */
+    const submitRegistInfo=({address, type, os, phoneinfo})=>api.put('',{address, type, os, phoneinfo});
+    /**
+     * 用户基本信息获取接口
+     *
+     * address   用户地址
+     */
+    const getUserInfo=({address})=>api.get('',{address});
+    /**
+     * Banner信息接口
+     */
+    const getBanner=()=>api.get('');
+    /**
+     * 系统配置信息接口
+     */
+    const getSystemConfig = () =>api.get('');
+    /**
+     * token列表接口
+     */
+    const getERC20Token = () =>api.get('');
+
     return {
-    // a list of the API functions from step 2
+        submitRegistInfo,
+        getUserInfo,
+        getBanner,
+        getSystemConfig,
+        getERC20Token,
+
+
         getRoot,
         getRate,
-        getUser
+        getUser,
     };
 };
 
-// let's return back our create method as the default.
 export default {
     create
 };
