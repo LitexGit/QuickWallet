fd24e7f72cea8d8cf5329d3a7f4b20c4b6974195
zhoujian <jian.zhou@litex.io>
2019-01-17 13:20:39 +0800

    feat(update-app okgo okhttp-utils): android 添加自动更新 功能
    android 添加自动更新 功能

--- android/app/src/main/java/com/quickwallet/MainActivity.java
+++ android/app/src/main/java/com/quickwallet/MainActivity.java
@@ -1,12 +1,36 @@
 package com.quickwallet;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.widget.Toast;
+
 import com.facebook.react.ReactActivity;
 
       import com.facebook.react.ReactActivityDelegate;
       import com.facebook.react.ReactRootView;
-      import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.quickwallet.utils.update.HProgressDialogUtils;
+import com.quickwallet.utils.update.OkGoUpdateHttpUtil;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.vector.update_app.UpdateAppBean;
+import com.vector.update_app.UpdateAppManager;
+import com.vector.update_app.UpdateCallback;
+import com.vector.update_app.service.DownloadService;
+import com.vector.update_app.utils.AppUpdateUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends ReactActivity {
+
+    private String mUpdateUrl = com.quickwallet.BuildConfig.ANDROID_UPDATE_URL;
+    private boolean isShowDownloadProgress;
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
@@ -26,4 +50,260 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "QuickWallet";
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        updateDiy2();
+    }
+
+    /**
+     * 自定义接口协议+自定义对话框
+     *
+     * @param
+     */
+    public void updateDiy2() {
+        //不显示下载进度
+        isShowDownloadProgress = false;
+        diyUpdate();
+    }
+
+
+    private void diyUpdate() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("appKey", "ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f");
+        params.put("appVersion", AppUpdateUtils.getVersionName(MainActivity.this));
+        params.put("appName", AppUpdateUtils.getAppName(MainActivity.this));
+        params.put("versionCode", "" + AppUpdateUtils.getVersionCode(MainActivity.this));
+        params.put("package", "" + getApplicationContext().getPackageName());
+
+
+        new UpdateAppManager
+                .Builder()
+                //必须设置，当前Activity
+                .setActivity(this)
+                //必须设置，实现httpManager接口的对象
+                .setHttpManager(new OkGoUpdateHttpUtil())
+                //必须设置，更新地址
+                .setUpdateUrl(mUpdateUrl)
+
+                //以下设置，都是可选
+                //设置请求方式，默认get
+                .setPost(false)
+                //添加自定义参数，默认version=1.0.0（app的versionName）；apkKey=唯一表示（在AndroidManifest.xml配置）
+                .setParams(params)
+                //设置apk下砸路径，默认是在下载到sd卡下/Download/1.0.0/test.apk
+                .setTargetPath(path)
+                //设置appKey，默认从AndroidManifest.xml获取，如果，使用自定义参数，则此项无效
+//                .setAppKey("ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f")
+
+                .build()
+                //检测是否有新版本
+                .checkNewApp(new UpdateCallback() {
+                    /**
+                     * 解析json,自定义协议
+                     *
+                     * @param json 服务器返回的json
+                     * @return UpdateAppBean
+                     */
+                    @Override
+                    protected UpdateAppBean parseJson(String json) {
+                        UpdateAppBean updateAppBean = new UpdateAppBean();
+                        try {
+                            JSONObject jsonObject = new JSONObject(json);
+                            updateAppBean
+                                    //（必须）是否更新Yes,No
+                                    .setUpdate(jsonObject.optString("update"))
+                                    //（必须）新版本号，
+                                    .setNewVersion(jsonObject.optString("new_version"))
+                                    //（必须）下载地址
+                                    .setApkFileUrl(jsonObject.optString("apk_file_url"))
+                                    //（必须）更新内容
+                                    .setUpdateLog(jsonObject.optString("update_log"))
+                                    //大小，不设置不显示大小，可以不设置
+                                    .setTargetSize(jsonObject.optString("target_size"))
+                                    //是否强制更新，可以不设置
+                                    .setConstraint(false)
+                                    //设置md5，可以不设置
+                                    .setNewMd5(jsonObject.optString("new_md5"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        return updateAppBean;
+                    }
+
+                    /**
+                     * 有新版本
+                     *
+                     * @param updateApp        新版本信息
+                     * @param updateAppManager app更新管理器
+                     */
+                    @Override
+                    public void hasNewApp(UpdateAppBean updateApp, UpdateAppManager updateAppManager) {
+                        //自定义对话框
+                        showDiyDialog(updateApp, updateAppManager);
+                    }
+
+                    /**
+                     * 网络请求之前
+                     */
+                    @Override
+                    public void onBefore() {
+//                        CProgressDialogUtils.showProgressDialog(MainActivity.this);
+                    }
+
+                    /**
+                     * 网路请求之后
+                     */
+                    @Override
+                    public void onAfter() {
+//                        CProgressDialogUtils.cancelProgressDialog(MainActivity.this);
+                    }
+
+                    /**
+                     * 没有新版本
+                     */
+                    @Override
+                    public void noNewApp(String error) {
+//                        Toast.makeText(MainActivity.this, "没有新版本", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    /**
+     * 自定义对话框
+     *
+     * @param updateApp
+     * @param updateAppManager
+     */
+    private void showDiyDialog(final UpdateAppBean updateApp, final UpdateAppManager updateAppManager) {
+        String targetSize = updateApp.getTargetSize();
+        String updateLog = updateApp.getUpdateLog();
+
+        String msg = "";
+
+        if (!TextUtils.isEmpty(targetSize)) {
+            msg = "新版本大小：" + targetSize + "\n\n";
+        }
+
+        if (!TextUtils.isEmpty(updateLog)) {
+            msg += updateLog;
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(String.format("是否升级到%s版本？", updateApp.getNewVersion()))
+                .setMessage(msg)
+                .setPositiveButton("升级", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //显示下载进度
+                        if (isShowDownloadProgress) {
+                            updateAppManager.download(new DownloadService.DownloadCallback() {
+                                @Override
+                                public void onStart() {
+                                    HProgressDialogUtils.showHorizontalProgressDialog(MainActivity.this, "下载进度", false);
+                                }
+
+                                /**
+                                 * 进度
+                                 *
+                                 * @param progress  进度 0.00 -1.00 ，总大小
+                                 * @param totalSize 总大小 单位B
+                                 */
+                                @Override
+                                public void onProgress(float progress, long totalSize) {
+                                    HProgressDialogUtils.setProgress(Math.round(progress * 100));
+                                }
+
+                                /**
+                                 *
+                                 * @param total 总大小 单位B
+                                 */
+                                @Override
+                                public void setMax(long total) {
+
+                                }
+
+
+                                @Override
+                                public boolean onFinish(File file) {
+                                    HProgressDialogUtils.cancel();
+                                    return true;
+                                }
+
+                                @Override
+                                public void onError(String msg) {
+                                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                                    HProgressDialogUtils.cancel();
+
+                                }
+
+                                @Override
+                                public boolean onInstallAppAndAppOnForeground(File file) {
+                                    return false;
+                                }
+                            });
+                        } else {
+                            //不显示下载进度
+                            updateAppManager.download();
+                        }
+
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("暂不升级", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
